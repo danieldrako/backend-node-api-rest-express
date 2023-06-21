74941f14c487fd750a537670c61289d3e52b9e66
Luis Daniel Ortiz <ls.ortizdanieldeveloper@gmail.com>
2023-06-21 15:07:49 -0600

    Changes to create the index.js to render routes with req and res to return responses on json and send format

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('My App')
+const express = require('express');
+const app =  express();
+const port = 3000;
+
+app.get('/', (req,res) => {
+  res.send('Hola mi server con express')
+ })
+app.get('/nueva-ruta', (req,res) => {
+  res.send('Hola este es un nuevo endpoint')
+ })
+app.get('/products', (req,res) => {
+  res.json({
+    name: 'Producto 1',
+    price: 1000
+  })
+ })
+
+ app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+  })
