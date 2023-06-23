8e473e1a7beda7d943a72995b500f35984d28cc0
Luis Daniel Ortiz <ls.ortizdanieldeveloper@gmail.com>
2023-06-23 09:33:38 -0600

    changes to add more endpoints with params and query

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
 const express = require('express');
+const {faker} = require('@faker-js/faker')
+
 const app =  express();
 const port = 3000;
 
 app.get('/', (req,res) => {
   res.send('Hola mi server con express')
  })
+
 app.get('/nueva-ruta', (req,res) => {
   res.send('Hola este es un nuevo endpoint')
  })
+
 app.get('/products', (req,res) => {
-  res.json({
-    name: 'Producto 1',
-    price: 1000
+  const products = [];
+  const {size} = req.query;
+  const limit = size || 10
+  for(let index = 0; index < limit; index++ ){
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(),10),
+      image: faker.image.url(),
+    })
+  }
+  res.json(products);
+});
+
+ app.get('/products/:id', (req,res) => {
+    const id = req.params.id;
+    res.json({
+      id,
+      name: 'Product 2',
+      price: 2000
+    })
   })
- })
+
+  app.get('/users',(req,res) => {
+    const limit = req.query.limit;
+    const offset = req.query.offset;
+    if(limit && offset) {
+      res.json({
+        limit,
+        offset
+      });
+    } else {
+      res.send('No hay parametros')
+    }
+   })
+//Usando las destructuracion
+ app.get('/categories/:categoryId/peoducts/:productId', (req,res) => {
+    const { categoryId, productId} = req.params;
+    res.json({
+      categoryId,
+      productId
+    });
+  })
 
  app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
