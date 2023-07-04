003b30a867e51634a9635389df0cd6da3f343456
Luis Daniel Ortiz <ls.ortizdanieldeveloper@gmail.com>
2023-07-04 09:25:29 -0600

    implement patch an delete methods in products

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,41 +1,59 @@
 const express = require('express');
-const {faker} = require('@faker-js/faker')
+const { faker } = require('@faker-js/faker');
 
 const router = express.Router();
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
   const products = [];
-  const {size} = req.query;
-  const limit = size || 10
-  for(let index = 0; index < limit; index++ ){
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
     products.push({
       name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(),10),
+      price: parseInt(faker.commerce.price(), 10),
       image: faker.image.url(),
-    })
+    });
   }
   res.json(products);
 });
 
-router.get('/filter' , (req,res) => {
-  res.send('Yo soy un filter')
- })
+router.get('/filter', (req, res) => {
+  res.send('Yo soy un filter');
+});
 
- router.get('/:id', (req,res) => {
-    const id = req.params.id;
-    res.json({
-      id,
-      name: 'Product 2',
-      price: 2000
-    })
-  })
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 2000,
+  });
+});
 
-  router.post('/', (req,res) => {
-    const body = req.body;
-    res.json({
-      message: 'created',
-      data: body
-    });
-   })
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.status(201).json({
+    message: 'created',
+    data: body,
+  });
+});
 
-  module.exports = router;
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'delete',
+    id,
+  });
+});
+
+module.exports = router;
