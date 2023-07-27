b4acef36901a1e90ec7a04d5d440553bd0518d66
Luis Daniel Ortiz <ls.ortizdanieldeveloper@gmail.com>
2023-07-26 23:51:47 -0600

    Introducción a servicios: crea tu primer servicio

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+
+const ProductService = require('./../services/product.service')
 
 const router = express.Router();
+const service = new ProductService();
 
 router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.url(),
-    });
-  }
+  const products = service.find();
   res.json(products);
 });
 
@@ -23,11 +16,8 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const id = req.params.id;
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 2000,
-  });
+  const product = service.findOne(id);
+  res.json(product);
 });
 
 router.post('/', (req, res) => {
